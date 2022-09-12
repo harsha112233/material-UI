bab6b3d0b78900b8ce42fd7e753d9efac640f989
harishm7 <hmateti@digisprint.com>
2022-09-12 11:04:45 +0530

    grid

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,53 +1,23 @@
-import {AppBar, Button, Container, ImageList, ImageListItem, TextField, Toolbar, Typography} from '@material-ui/core'
-import ImageData from './assets/ImageData.json'
-import {makeStyles} from '@material-ui/core/styles'
-import CameraEnhanceIcon from '@material-ui/icons/CameraEnhance';
 
-const useStyles=makeStyles({
-  text:{
-    "marginTop":"70px",
-    "textAlign":"center",
-    "color":"green"
-  }
-})
+import React, { Suspense } from 'react'
+import { Grid } from '@material-ui/core'
+const Home=React.lazy(()=>import("./Home"))
+
+
 function App() {
-  const classes=useStyles();
+
   return (
     <div className="App">
-      <AppBar>
-        <Toolbar>
-          <Typography variant='h4'><CameraEnhanceIcon />Photography</Typography>
-        </Toolbar>
-      </AppBar>
-      <Container className={classes.text}>
-        <Typography variant='h2'>Our latest version of Photography</Typography>
-        <Typography variant='paragraph'>Pleaser to take your photos as well please fill the below form  </Typography>
-      </Container>
-      <Container style={{"marginTop":"70px"}}>
-        <ImageList rowHeight={140} cols={4}>
-          {
-            ImageData.map((item,ind)=>{
-              return(
-                <>
-                <ImageListItem cols={1}><img src={item.imgurl} alt="" /></ImageListItem>
-                </>
-              )
-            })
-          }
-        </ImageList>
-      </Container>
-      <center>
-        <Container style={{"marginTop":"70px"}}>
-          <Typography variant='h3'>Contact Form</Typography>
-          <form>
-            <TextField style={{"width":"500px","margin":"10px"}} type="text" label="Name" variant="outlined" /><br />
-            <TextField style={{"width":"500px","margin":"10px"}} type="email" label="Email" variant="outlined" /><br />
-            <TextField style={{"width":"500px","margin":"10px"}} type="text" label="Address" variant="outlined" /><br />
-            <TextField style={{"width":"500px","margin":"10px"}} type="text" label="praposal" variant="outlined" /><br />
-            <Button variant="contained" color="primary">Submit</Button>
-          </form>
-        </Container>
-      </center>
+    <Suspense fallback={<h1>loading</h1>}>
+     {/* <Home /> */}
+     <Grid lg={12} item container spacing={2}>
+      <Grid item lg={3} xs={12} sm={6}  style={{backgroundColor:"yellow"}}>Block 1</Grid>
+      <Grid item lg={3} xs={12} sm={6} style={{backgroundColor:"yellow"}}>Block 2</Grid>
+      <Grid item lg={3} xs={12} sm={6} style={{backgroundColor:"yellow"}} >Block 3</Grid>
+      <Grid item lg={3} xs={12} sm={6} style={{backgroundColor:"yellow"}}>Block 4</Grid>
+     </Grid>
+    </Suspense>
+     
     </div>
   )
 }
---
+++ vite-project/src/Home.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import {AppBar, Button, Container, ImageList, ImageListItem, TextField, Toolbar, Typography} from '@material-ui/core'
+import ImageData from './assets/ImageData.json'
+import {makeStyles} from '@material-ui/core/styles'
+import CameraEnhanceIcon from '@material-ui/icons/CameraEnhance';
+
+const useStyles=makeStyles({
+    text:{
+      "marginTop":"70px",
+      "textAlign":"center",
+      "color":"green"
+    }
+  })
+const Home = () => {
+    const classes=useStyles();
+  return (
+    <div>
+       <AppBar>
+        <Toolbar>
+          <Typography variant='h4'><CameraEnhanceIcon />Photography</Typography>
+        </Toolbar>
+      </AppBar>
+      <Container className={classes.text}>
+        <Typography variant='h2'>Our latest version of Photography</Typography>
+        <Typography variant='paragraph'>Pleaser to take your photos as well please fill the below form  </Typography>
+      </Container>
+      <Container style={{"marginTop":"70px"}}>
+        <ImageList rowHeight={140} cols={4}>
+          {
+            ImageData.map((item,ind)=>{
+              return(
+                <>
+                <ImageListItem cols={1}><img src={item.imgurl} alt="" /></ImageListItem>
+                </>
+              )
+            })
+          }
+        </ImageList>
+      </Container>
+      <center>
+        <Container style={{"marginTop":"70px"}}>
+          <Typography variant='h3'>Contact Form</Typography>
+          <form>
+            <TextField style={{"width":"500px","margin":"10px"}} type="text" label="Name" variant="outlined" /><br />
+            <TextField style={{"width":"500px","margin":"10px"}} type="email" label="Email" variant="outlined" /><br />
+            <TextField style={{"width":"500px","margin":"10px"}} type="text" label="Address" variant="outlined" /><br />
+            <TextField style={{"width":"500px","margin":"10px"}} type="text" label="praposal" variant="outlined" /><br />
+            <Button variant="contained" color="primary">Submit</Button>
+          </form>
+        </Container>
+      </center>
+    </div>
+  );
+}
+
+export default Home;
